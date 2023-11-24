066ee1afa3669737ff74387c114355c19473aa93
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-24 17:55:37 +0100

    add: button function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 // creo il mio arrey con la lista della spesa
 
-let listaSpesa = ["pasta", "uova", "guanciale", "formaggio"];
+const listaSpesa = ["pasta", "uova", "guanciale", "formaggio"];
 
 let containerList = document.getElementById ('spesa');
+
+
 // scrollo la mia lisa
 
 let i = 0;
 
 while (i < listaSpesa.length){
+   
 
     console.log(listaSpesa [i]);
 
@@ -18,6 +21,36 @@ while (i < listaSpesa.length){
     elementList.append (listaSpesa [i]);
 
     containerList.append (elementList);
-    
+
     i++;
 }
+//    creiamo le costanti per l'imput e il bottone
+    const inputUser = document.getElementById ('myspesa');
+    
+    const sendButton = document.getElementById ('button');
+
+
+    // creiamo la funzione del bottone
+
+    sendButton.addEventListener ('click',
+    function(){
+
+        // aggiungiamo all'Array i valori dell'input
+    
+    listaSpesa.push(inputUser.value);
+
+    console.log(listaSpesa);
+
+    // aggiungiamo i valori assegnati alla lista
+
+    const addList = document.createElement ('li')
+
+    addList.append (inputUser.value);
+
+    containerList.append (addList);
+
+    
+    }); 
+
+
+
